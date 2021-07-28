19ad0ae844eee5fa119c6d68e5b85bed7dc3171c
Christopher Woods <chryswoods@hey.com>
2021-07-28 11:14:16 +0100

    Cleaning up the routing

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
 
-import './App.css';
+import React from 'react';
 
-import Webpages from './WebPages';
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch
+} from "react-router-dom";
+
+import Home from './Home';
+import Interstitial from './interstitial/Interstitial';
+import Search from './search/Search';
 
 function App() {
   return (
     <div>
-      <Webpages />
+        <Router basename={process.env.PUBLIC_URL}>
+            <Switch>
+              <Route exact path="/" component = {Home} />
+              <Route exact path="/interstitial" component = {Interstitial} />
+              <Route exact path="/search" component = {Search} />
+              <Route render={() => <h1>404: page not found</h1>} />
+            </Switch>
+        </Router>
     </div>
   );
 }
