4d92b7762ea96895621c4c8ad82093364fd187c4
Christopher Woods <chryswoods@hey.com>
2021-07-31 18:39:33 +0100

    Cleaning up more - getting the navbar in place.

--- src/Navigation.js
+++ src/Navigation.js
@@ -1,21 +1,43 @@
 
 import React from 'react';
 
-import styles from "./Navigation.module.css";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import Container from "react-bootstrap/Container";
 
-const Navigation = () => {
+import {LinkContainer} from 'react-router-bootstrap'
+
+function Navigation(props){
+
+  let account = props.account;
+
+  let login_logout = null;
+
+  if (account && account.isLoggedIn()){
+    login_logout = (
+      <LinkContainer to="/logout">
+        <Nav.Link>Logout</Nav.Link>
+      </LinkContainer>
+    );
+  } else {
+    login_logout = (
+      <LinkContainer to="/login">
+        <Nav.Link>Login</Nav.Link>
+      </LinkContainer>
+    );
+  }
+
   return (
-    <div className={styles.navbar}>
-      <div className={styles.navitem}>
-        <a href="https://septembrse.github.io/app/#/app/search">Timetable</a>
-      </div>
-      <div className={styles.navitem}>
-        <a href="https://septembrse.github.io">Venue Guide</a>
-      </div>
-      <div className={styles.navitem}>
-        <a href="https://septembrse.society-rse.org">Website</a>
-      </div>
-    </div>
+    <Navbar expand="lg">
+      <Container>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            {login_logout}
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
   );
 }
 
--- src/SimplePage.js
+++ src/SimplePage.js
@@ -6,20 +6,17 @@ import Navigation from './Navigation';
 
 import styles from "./SimplePage.module.css";
 
-class SimplePage extends React.Component {
-
-  render(){
-    return (
-      <div className={styles.page}>
-        <Banner />
-        <Navigation />
-        <div className={styles.content}>
-          {this.props.children}
-        </div>
-        <div className={styles.background}/>
+function SimplePage(props){
+  return (
+    <div className={styles.page}>
+      <Banner />
+      <Navigation account={props.account}/>
+      <div className={styles.content}>
+        {this.props.children}
       </div>
-    );
-  }
+      <div className={styles.background}/>
+    </div>
+  );
 };
 
 export default SimplePage;
