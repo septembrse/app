c0c4513874e4f752f4f15a3196aeea98db535f30
Christopher Woods <chryswoods@hey.com>
2021-08-01 08:04:48 +0100

    Fixed bug in SimplePage

--- src/SimplePage.js
+++ src/SimplePage.js
@@ -6,17 +6,20 @@ import Navigation from './Navigation';
 
 import styles from "./SimplePage.module.css";
 
-function SimplePage(props){
-  return (
-    <div className={styles.page}>
-      <Banner />
-      <Navigation account={props.account}/>
-      <div className={styles.content}>
-        {this.props.children}
+class SimplePage extends React.Component {
+
+  render(){
+    return (
+      <div className={styles.page}>
+        <Banner />
+        <Navigation account={this.props.account} />
+        <div className={styles.content}>
+          {this.props.children}
+        </div>
+        <div className={styles.background}/>
       </div>
-      <div className={styles.background}/>
-    </div>
-  );
+    );
+  }
 };
 
 export default SimplePage;
