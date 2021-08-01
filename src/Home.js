1bbc89e1e190df4714cd18acd0a12c646b8ef027
Christopher Woods <chryswoods@hey.com>
2021-08-01 10:59:00 +0100

    Made a nicer home page

--- src/Home.js
+++ src/Home.js
@@ -1,20 +1,54 @@
 
 import React from 'react';
 
-import { Link } from "react-router-dom";
+import SimplePage from "./SimplePage";
+
+import Account from './model/Account';
+
+import ButtonGroup from "react-bootstrap/ButtonGroup";
+import Button from "react-bootstrap/Button";
 
 //import styles from "./Home.css"
 
-const Home = () => {
-    return (
-        <div>
-            <h1>SeptembRSE</h1>
-            <div><Link to="/interstitial">Interstitial</Link></div>
-            <div><Link to="/search">Search</Link></div>
-            <div><Link to="/login">Login</Link></div>
-            <div><Link to="/logout">Logout</Link></div>
-            <div><Link to="/generate">Generate</Link></div>
-        </div>
-    );
+function Home(props){
+
+  let [account, setAccount] = React.useState(null);
+
+  React.useEffect(() => {
+    setAccount(Account.get_account());
+  }, [account]);
+
+  return (
+    <SimplePage account={account} setAccount={setAccount}>
+      <h1>SeptembRSE</h1>
+      <h2>Conference Information System</h2>
+      <ButtonGroup vertical style={{width: "80%"}}>
+        <Button onClick={() => props.history.push("/venue")}
+                variant="primary">
+          Guide to the conference venue (gather.town)
+        </Button>
+        <Button onClick={() => props.history.push("/today")}
+                variant="secondary">
+          What's happening today?
+        </Button>
+        <Button onClick={() => props.history.push("/timetable")}
+                variant="info">
+          View the conference timetable
+        </Button>
+        <Button onClick={() => props.history.push("/search")}
+                variant="secondary">
+          Search for a presentation or event
+        </Button>
+        <Button onClick={() => props.history.push("/ticket")}
+                variant="info">
+          View your conference ticket
+        </Button>
+        <Button href="https://septembrse.society-rse.org"
+                variant="primary">
+          Go to the conference website
+        </Button>
+      </ButtonGroup>
+    </SimplePage>
+  );
 };
 export default Home;
